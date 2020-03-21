377d61dfce483fa66761c994959434342fba2bf6
Ashish-Bridgelabz <asaschoudhary49@gmail.com>
2020-03-22 02:44:30 +0530

    REFACTOR:Player rolls the diec to get a number between 1 to 6

--- SnakeLadderProblem/snakeLadder.sh
+++ SnakeLadderProblem/snakeLadder.sh
@@ -2,5 +2,11 @@
 #DISPLAY WELCOME STATEMENT
 echo "Welcome to the snake and ladder problem"
 #PLAYER1 STARTING POSITION
-PLAYER1=0
+player1=0
+position=0
+#GENERATE THE RANDOM NUMBER FOR NEW POSITION OR SAME POSITION 
+playerRollsDiec=$((RANDOM%6+1))
+
+
+
 
