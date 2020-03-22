379e9825a5b6224ef0f20ae1a4ae69332130720b
Ashish-Bridgelabz <asaschoudhary49@gmail.com>
2020-03-22 22:40:22 +0530

    MODIFIED:Repeated till the player reached the winning position 100

--- SnakeLadderProblem/snakeLadder.sh
+++ SnakeLadderProblem/snakeLadder.sh
@@ -1,27 +1,37 @@
 #!/bin/bash -x
 #DISPLAY WELCOME STATEMENT
 echo "Welcome to the snake and ladder problem"
-#PLAYER1 STARTING POSITION
+#START A GAME WITH SINGLE PLAYER WITH ZERO POSITION
+#PLAYER1 STARTING POSITION ZERO
 player1=0
 position=0
 NoPlay=0
 Ladder=1
 Snake=2
-#GENERATE THE RANDOM NUMBER FOR NEW POSITION OR SAME POSITION 
-playerRollsDiec=$((RANDOM%6+1))
-#GENERATE THE RANDOM VALUE AND CHECK THE OPTION TO AUTOMATIC
-checkOption=$((RANDOM%3))
-case  $checkOption in
-   $NoPlay)
-         position=$position
-         echo "Stay in the same:"$position;;
-   $Ladder)
-         position=$(($position+$playerRollsDiec))
-         echo "Count a new ladder position:"$position;;
-   $Snake)
-         position=$(($position+$playerRollsDiec))
-         echo "Count a new snake position:"$position;;
-esac
+while (($position!=100))
+do
+	#GENERATE THE RANDOM NUMBER FOR NEW POSITION OR SAME POSITION 
+	playerRollsDiec=$((RANDOM%6+1))
+	#GENERATE THE RANDOM VALUE AND CHECK THE OPTION TO AUTOMATIC
+	checkOption=$((RANDOM%3))
+	case  $checkOption in
+   	$NoPlay)
+					position=$position
+					echo "Stay in a same poisition:"$position;;
+   	$Ladder)
+      		   position=$(($position+$playerRollsDiec))
+					echo "player moves ahead by position:"$position;;
+   	$Snake)
+         		position=$(($position-$playerRollsDiec))
+					echo "player moves behind by position:"$position
+					if(($position<=0))
+					then
+					position=0
+					fi;;
+	esac
+done
+
+
 
 
 
