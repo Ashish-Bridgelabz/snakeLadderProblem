b41145c9776bc3e8df9c0db6cc90e84d518abf51
Ashish-Bridgelabz <asaschoudhary49@gmail.com>
2020-03-22 10:00:43 +0530

    MODIFIED:Checked option number of player Ladder Snake and Noplay

--- SnakeLadderProblem/snakeLadder.sh
+++ SnakeLadderProblem/snakeLadder.sh
@@ -4,8 +4,25 @@ echo "Welcome to the snake and ladder problem"
 #PLAYER1 STARTING POSITION
 player1=0
 position=0
+NoPlay=0
+Ladder=1
+Snake=2
 #GENERATE THE RANDOM NUMBER FOR NEW POSITION OR SAME POSITION 
 playerRollsDiec=$((RANDOM%6+1))
+#GENERATE THE RANDOM VALUE AND CHECK THE OPTION TO AUTOMATIC
+checkOption=$((RANDOM%3))
+case  $checkOption in
+   $NoPlay)
+         position=$position
+         echo "Stay in the same:"$position;;
+   $Ladder)
+         position=$(($position+$playerRollsDiec))
+         echo "Count a new ladder position:"$position;;
+   $Snake)
+         position=$(($position+$playerRollsDiec))
+         echo "Count a new snake position:"$position;;
+esac
+
 
 
 
