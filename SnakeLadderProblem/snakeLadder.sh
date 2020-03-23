44d6baf45313d6091c62c7e31cd69d9cfea291d7
Ashish-Bridgelabz <asaschoudhary49@gmail.com>
2020-03-23 17:39:38 +0530

    RENAME:Play the game with two player and which player won the game

--- SnakeLadderProblem/snakeLadder.sh
+++ SnakeLadderProblem/snakeLadder.sh
@@ -1,53 +1,64 @@
 #!/bin/bash -x
-#DISPLAY WELCOME STATEMENT
-echo "Welcome to the snake and ladder problem"
-#START A GAME WITH SINGLE PLAYER WITH ZERO POSITION
-#PLAYER1 STARTING POSITION ZERO
-player1=0
+echo "Welcome to snake and ladder problem"
+#START A GAME WITH PLAYER1 AND PLAYER2 WITH ZERO POSITION
+#VARIABLES
 position=0
+player1=0
+player2=0
+diecount=0
+diecount2=0
 NoPlay=0
 Ladder=1
 Snake=2
-declare -A diecPosition
-#WINNING CONDITION
-while (($position!=100))
-do
-	diecount=$((diecount+1))
-	#GENERATE THE RANDOM NUMBER FOR NEW POSITION OR SAME POSITION 
+declare -A diePosition
+function snakeGame()
+{
+	position=$1
+	##GENERATE THE RANDOM NUMBER FOR NEW POSITION OR SAME POSITION
 	playerRollsDie=$((RANDOM%6+1))
-	#GENERATE THE RANDOM VALUE AND CHECK THE OPTION TO AUTOMATIC
+	#GENERATE THE RANDOM VALUE AND CHECK THE OPTION 
 	checkOption=$((RANDOM%3))
-	case  $checkOption in
-   	$NoPlay)
-				position=$position
-				echo "Stay in a same poisition:"$position;;
-   	$Ladder)
+	case $checkOption in
+		$NoPlay)
+				position=$position;;
+				
+		$Ladder)
 				#EXACT WINNING POSITION
-				if (($(($position+$playerRollsDie))>100))
+				if(( $((position+playerRollsDie))>100 ))
 				then
 					position=$position
-					echo "player staying in the same position"$position
 				else
-					position=$(($position+$playerRollsDie))
-					echo "player stay in a new position:"$position
-					fi;;
-		$Snake)
-				position=$(($position-$playerRollsDie))
-				echo "player moves behind by position:"$position
-				if(($position<=0))
-				then
-				position=0
+					position=$((position+playerRollsDie))
+					position=$position
 				fi;;
+		$Snake)
+				if(($position>$playerRollsDie))
+				then
+					position=$((position-playerRollsDie))
+					position=$position
+				else
+					position=0
+				fi
+				;;
 	esac
 	#COUNT THE DIECE AND POSITION
-	diecPosition[$diecount]=$position
-	echo "Diecount $diecount: Position $position"
-	#echo $dicePosition
+	diePosition[$diecount]=$position
+	echo $position
+}
+#TILL PLAY 100 FOR BOTH PLAYER
+while(($player1!=100 && $player2!=100))
+do
+	((diecount++))
+	player1="$(snakeGame $player1)"
+	echo "player1 $diecount : Position $player1"
+	((diecount2++))
+	player2="$(snakeGame $player2)"
+	echo "player2 $diecount2 : Position $player2"
 done
-
-
-
-
-
-
-
+#THIS IS USE FOR WINNING POSITION
+if(($player1==100))
+then
+	echo "Congratulations!!! Player1 Won the diecount $diecount times"
+else
+	echo "Congratulations!!!! Player2 Won the diecount $diecount2 times"
+fi
